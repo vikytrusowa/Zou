22762d5269335b89e025a3b6fd39761a161570b3
vikytrusowa <vikytrusowa.@gmail.com>
2019-01-04 15:27:24 +0200

    two commit

--- gulpfile.js
+++ gulpfile.js
@@ -8,21 +8,21 @@ const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-htmlmin');
 
 gulp.task('default', function () {
-    return gulp.src('src/css/*.css')
-        .pipe(concatCss("styles/style.css"))
+    return gulp.src('src/style/css/*.css')
+        .pipe(concatCss("style/css/style.css"))
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest('static/'));
 });
 gulp.task('scripts', function() {
     return gulp.src('src/js/*.js')
-        .pipe(concat('js/all.js'))
+        .pipe(concat('js/main.js'))
         .pipe(uglyfly())
         .pipe(gulp.dest('static/'));
 });
 gulp.task('image', () =>
     gulp.src('src/img/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('static/images'))
+        .pipe(gulp.dest('static/img'))
 );
 gulp.task('minify', () => {
     return gulp.src('src/*.html')
--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,7 @@
 
     $('.slider').slick({
         slidesToShow: 2,
+        infinite: false,
         slide: "div",
         prevArrow: ".prev",
         nextArrow: ".next",
@@ -21,10 +22,58 @@
                     slidesToShow: 1
                 }
             }
-            ]
+        ]
 
     });
 
+    $('.js-button').on('click', function () {
+
+        var element = $('.slide.slick-active').first();
+        var position = element.data('slick-index');
+        var route = $(this).data('route');
+        if (route === 'next') {
+            var firsElementPosition = position - 1;
+            var lastElementPosition = position + 1;
+
+            $('.slide').map(function (index, object) {
+                var selector = $(object);
+
+
+                if (selector.data('slick-index') === firsElementPosition) {
+                    selector.addClass('slider__slide--prev');
+
+                }
+                if (selector.data('slick-index') === lastElementPosition) {
+                    selector.removeClass('slider__slide--prev');
+                    selector.removeClass('slider__slide--next');
+
+
+                }
+            })
+        } else {
+            var firsElementPosition = position + 2;
+            var lastElementPosition = position;
+            $('.slide').map(function (index, object) {
+                var selector = $(object);
+
+
+                if (selector.data('slick-index') === firsElementPosition) {
+                    selector.addClass('slider__slide--next');
+
+                }
+                if (selector.data('slick-index') === lastElementPosition) {
+                    selector.removeClass('slider__slide--next');
+                    selector.removeClass('slider__slide--prev');
+
+
+                }
+            })
+        }
+
+
+
+
+    })
 })(jQuery);
 
 
